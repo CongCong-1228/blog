11f463b44002b89527148182a5d4397b8da86d73
mitani <fivesun1228@163.com>
2025-01-10 17:05:52 +0800

    update: finish home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="w-full h-full flex justify-center items-center text-center mt-16">
+    <div className="w-full h-full flex justify-center items-center text-center">
       <div className="flex flex-col items-center">
-        <div className="flex items-center w-48 h-48 rounded-[50%] border-2 border-gray-300 dark:border-gray-700">
+        <div className="relative h-64 w-64 rounded-full border-2 border-gray-300 dark:border-gray-700 mb-4 object-cover">
           <Image
-            src="/vercel.svg"
+            src="/avatar.jpg"
             alt="avatar"
-            priority
-            width={192}
-            height={192}
+            className="object-cover rounded-full"
+            layout="fill"
           />
         </div>
-        <h1 className="text-2xl font-bold">Mitani</h1>
-        <p className="text-gray-500 dark:text-gray-300">A software engineer</p>
+
+        <h1 className="text-2xl font-bold m-4">Mitani</h1>
+        <p className="text-gray-500 dark:text-gray-300">A frontend engineer</p>
       </div>
     </div>
   );
--- posts/a-chain-reaction/index.md
+++ posts/a-chain-reaction/index.md
@@ -4,4 +4,4 @@ date: "2025-01-03"
 spoiler: "The limits of my language mean the limits of my world."
 ---
 
-I wrote a bit of JSX in my editor.
+
