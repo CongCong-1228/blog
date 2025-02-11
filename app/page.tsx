7f8debc5b2aa1f3f814ecb6ae8b32a8ce528110d
mitani <fivesun1228@163.com>
2025-02-11 15:39:46 +0800

    fix Image components warning use fill replace layout fill

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
             src="/avatar.jpg"
             alt="avatar"
             className="object-cover rounded-full"
-            layout="fill"
+            fill
+            priority
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
 
